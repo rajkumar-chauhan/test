2be9229f6d56038afa2f1649d521b4b9255d46fc
rajkumar@123 <160397769+rajkumar-chauhan@users.noreply.github.com>
2024-08-08 14:50:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "custom_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "custom_vpc"
+  }
+}
